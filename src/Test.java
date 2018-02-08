111b03b21aefb2164376a50dd8673cfeefa90d38
Niccolò Maltoni <nicco.mlt@gmail.com>
2018-02-08 10:43:01 +0100

    Aggiunti file esercitazione 03/11/2017

--- src/Test.java
+++ src/Test.java
@@ -3,36 +3,41 @@ import org.antlr.v4.runtime.*;
 /**
  * Il seguente codice mette in esecuzione il tutto:
  * <p><ul>
- * <li>si aspetta che lo stream input sia presente in un file denominato prova.txt;
+ * <li>si aspetta che lo stream input sia presente in un file denominato quicksort.fool.asm;
  * <li>legge un {@link CharStream} dal file;
- * <li>passa il {@link CharStream} al Lexer;
- * <li>crea un {@link TokenStream} dal Lexer;
- * <li>passa il {@link TokenStream} al Parser;
- * <li>analizza il programma;
+ * <li>passa il {@link CharStream} al {@link SVMLexer};
+ * <li>crea un {@link CommonTokenStream} dal Lexer;
+ * <li>passa il {@link CommonTokenStream} al {@link SVMParser};
+ * <li>assembla il codice e stampa eventuali errori lessicali;
  * <li>stampa il resoconto.
  * </ul><p>
  */
 public class Test {
-	
+    
 	/**
-	 * Mette in esecuzione il codice, caricando la stringa da un file prova.txt.
+	 * Mette in esecuzione il codice, caricando la stringa da un file quicksort.fool.asm.
 	 * 
 	 * @param args argomenti del metodo main, inutili
 	 * @throws Exception nel caso in cui vengano tirate eccezioni per errori vari
 	 */
-    public static void main(String[] args) throws Exception {
-        String filename="prova.txt";
-
-        CharStream chars = CharStreams.fromFileName(filename);
-        SimpleExpLexer lexer = new SimpleExpLexer(chars);
-    	
+	public static void main(String[] args) throws Exception {
+     
+        String fileName = "quicksort.fool.asm";
+                
+        CharStream chars = CharStreams.fromFileName(fileName);
+        SVMLexer lexer = new SVMLexer(chars);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SimpleExpParser parser = new SimpleExpParser(tokens);
+        SVMParser parser = new SVMParser(tokens);
         
-        parser.prog();
+        parser.assembly();
         
-        System.out.println("You had: "+lexer.lexicalErrors+" lexical errors and "+
-                           parser.getNumberOfSyntaxErrors()+" syntax errors.");
+        System.out.println("You had: "+lexer.lexicalErrors+" lexical errors and "+parser.getNumberOfSyntaxErrors()+" syntax errors.");
+        if (lexer.lexicalErrors>0 || parser.getNumberOfSyntaxErrors()>0) System.exit(1);
+
+        System.out.println("Starting Virtual Machine...");
+        ExecuteVM vm = new ExecuteVM(parser.code);
+        vm.cpu();
         
+      
     }
 }
